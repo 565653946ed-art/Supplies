fc52877cdec2fe05fd1be717eab0052be073bb47
pc13 <pc13@example.com>
2026-02-18 11:57:37 +0900

    管理者権限追加

--- src/main/java/com/example/supplies/controller/ItemController.java
+++ src/main/java/com/example/supplies/controller/ItemController.java
@@ -1,5 +1,8 @@
 package com.example.supplies.controller;
 
+import jakarta.servlet.http.HttpSession;
+
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -12,7 +15,6 @@ import com.example.supplies.entity.Item;
 import com.example.supplies.security.LoginUser;
 import com.example.supplies.service.ItemService;
 
-import jakarta.servlet.http.HttpSession;
 import lombok.RequiredArgsConstructor;
 
 //Interceptorでログインチェック
@@ -45,6 +47,7 @@ public class ItemController {
 	}
 
 	//商品登録
+	@PreAuthorize("hasRole('ADMIN')")
 	@PostMapping("/add")
 	public String add(HttpSession session, Item item) {
 		itemService.addItem(item);
@@ -52,6 +55,7 @@ public class ItemController {
 	}
 
 	//製品削除
+	@PreAuthorize("hasRole('ADMIN')")
 	@PostMapping("/delete/{id}")
 	public String delete(@PathVariable Integer id, HttpSession session) {
 		itemService.deleteItem(id);
